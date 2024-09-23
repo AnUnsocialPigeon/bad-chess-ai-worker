30955cef12a088b21f81d86c5912ab02111492a3
AnUnsocialPigeon <jdturrell@gmail.com>
2024-09-23 21:24:28 +0100

    Added some (very basic) ai

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,9 @@
-import { Chess } from 'chess.js';
+import { Chess, PieceSymbol } from 'chess.js';
+
+interface MoveScore {
+	move: string;
+	score: number;
+}
 
 export default {
 	async fetch(request: Request): Promise<Response> {
@@ -22,7 +27,7 @@ export default {
 				const chess = new Chess(requestBody);
 
 				if (chess.isGameOver()) {
-					return new Response('Game over!', {
+					return new Response(`Game over!`, {
 						status: 400,
 						headers: {
 							'Access-Control-Allow-Origin': '*',
@@ -30,9 +35,60 @@ export default {
 					});
 				}
 
-				const moves = chess.moves();
-				const randomMove = moves[Math.floor(Math.random() * moves.length)];
-				chess.move(randomMove);
+				function shuffleMoves(moves: MoveScore[]): MoveScore[] {
+					for (let i = moves.length - 1; i > 0; i--) {
+						const j = Math.floor(Math.random() * (i + 1));
+						[moves[i], moves[j]] = [moves[j], moves[i]];
+					}
+					return moves;
+				}
+
+				const moves: MoveScore[] = chess.moves({ verbose: true }).map(move => {
+					// Score moves
+					let score = 0;
+
+					const tempChess = new Chess(move.after);
+					const pieceFrom = chess.get(move.from);
+					const pieceTo = chess.get(move.to);
+
+					const pieceScores: Record<PieceSymbol, number> = {
+						"p": 1,
+						"n": 3,
+						"b": 3,
+						"r": 5,
+						"q": 9,
+						"k": 20
+					}
+
+					if (move.san.includes('#')) score += 100;
+					if (move.san.includes('+')) score += 0.7;
+					if (move.san.includes('O-O')) score += 0.4;
+					if (move.promotion) score += 0.4;
+					if (move.piece === "k") score -= 0.2;
+
+					// Capture logic
+					if (pieceFrom && pieceTo) {
+						console.log(`${pieceFrom} -> ${pieceTo} = ${pieceScores[pieceFrom.type]} -> ${pieceScores[pieceTo.type]}`)
+						if (pieceScores[pieceFrom.type] <= pieceScores[pieceTo.type]) score += 0.7;
+					}
+
+					// Hung material logic
+					if (chess.isAttacked(move.from, move.color === "w" ? "b" : "w") && !chess.isAttacked(move.from, move.color))
+						score -= 0.3;
+
+					return {
+						move: move.san,
+						score: score
+					}
+				});
+
+				console.log(moves);
+				
+				const highestScore = Math.max(...moves.map(x => x.score));
+				const chosenMove = moves.filter((x) => x.score == highestScore);
+				
+				console.log(chosenMove);
+				chess.move(shuffleMoves(chosenMove)[0].move);
 
 				// Return the new FEN string
 				return new Response(chess.fen(), {
@@ -43,7 +99,7 @@ export default {
 					},
 				});
 			} catch (e) {
-				return new Response('Invalid FEN string', {
+				return new Response(`${e}`, {
 					status: 400,
 					headers: {
 						'Access-Control-Allow-Origin': '*',
@@ -53,7 +109,7 @@ export default {
 		}
 
 		// Return error for unsupported methods
-		return new Response('Bad request.', {
+		return new Response(`Bad request. ${request.method}`, {
 			status: 400,
 			headers: {
 				'Access-Control-Allow-Origin': '*',
